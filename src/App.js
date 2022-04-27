1a2bd3cc3e8674f888aae3b6452d86437f103e75
tommychong316 <chong316@gmail.com>
2022-04-27 15:28:15 -0400

    created useState + useEffect functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import DisplayPlatformStats from './components/DisplayPlatformStats';
 
 function App() {
+  
+  
+  const [videoGames, setVdieoGaems] = useState([])
+
+  useEffect(() => {
+    getAllVideoGames();
+  }, []);
+
+  async function getAllVideoGames(){
+    try{
+    debugger;
+    const response = await axios.get('https://localhost:7260/api/games/');
+    setVdieoGaems(response.data);
+  } catch(ex){
+      console.log(`ERROR in getVideoGames EXCEPTION: ${ex}`)
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+     <DisplayPlatformStats videoGames={videoGames}/>
     </div>
   );
 }
