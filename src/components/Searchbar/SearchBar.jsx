c51a689e4cf8ee60fd2a23486e43a8da2aeaf36d
tommychong316 <chong316@gmail.com>
2022-04-28 15:31:30 -0400

    created searchbar function

--- src/components/Searchbar/SearchBar.jsx
+++ src/components/Searchbar/SearchBar.jsx
@@ -1,7 +1,15 @@
-import React, { useState } from 'react';
+import React from 'react';
 
-const SearchBar = () => {
-    return (  );
+const SearchBar = ({searchTerm, setSearchTerm}) => {
+    const handleChange = (event) => {
+        setSearchTerm(event.target.value)
+    }
+    return ( 
+        <div>
+            <input type='text' placeholder='Search' value={searchTerm} onChange={handleChange} />
+            <button type='submit'>Search</button>
+        </div>
+     );
 }
  
 export default SearchBar;
