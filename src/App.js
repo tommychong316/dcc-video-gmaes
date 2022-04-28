14f208b0c93db76a1d033f4d36df8abd1cd68450
tommychong316 <chong316@gmail.com>
2022-04-28 16:10:45 -0400

    added display function to App.js folder

--- src/App.js
+++ src/App.js
@@ -1,29 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import DisplayPlatformStats from './components/DisplayPlatformStats';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import DisplayPlatformStats from "./components/DisplayPlatformStats";
+import "./App.css";
+import SearchBar from "./components/Searchbar/SearchBar";
+import DisplayGames from "./components/DsiplayGames/DisplayGames";
 
 function App() {
+  const [videoGames, setVideoGames] = useState([]);
+  const [searchTerm, setSearchTerm] = useState([""])
   
-  
-  const [videoGames, setVdieoGaems] = useState([])
-
   useEffect(() => {
     getAllVideoGames();
   }, []);
 
-  async function getAllVideoGames(){
-    try{
-    debugger;
-    const response = await axios.get('https://localhost:7260/api/games/');
-    setVdieoGaems(response.data);
-  } catch(ex){
-      console.log(`ERROR in getVideoGames EXCEPTION: ${ex}`)
+  async function getAllVideoGames() {
+    try {
+      debugger;
+      const response = await axios.get("https://localhost:7260/api/games/");
+      setVideoGames(response.data);
+    } catch (ex) {
+      console.log(`ERROR in getVideoGames EXCEPTION: ${ex}`);
     }
   }
 
   return (
-    <div>
-     <DisplayPlatformStats videoGames={videoGames}/>
+    <div className="container">
+      <div className="row"/>
+      
+        <h3 style={{margin: '1em'}}>GAME STATS</h3>
+          <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+          <DisplayPlatformStats searchTerm={searchTerm} videoGames={videoGames} />
+          <div className="border-box">
+          <DisplayGames parentVideoGames={videoGames} />
+          
+          </div>
+     
     </div>
   );
 }
